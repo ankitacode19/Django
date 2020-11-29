10f96ace7975d26440b5158c05b35bdee0b6bf79
ankitacode19 <ankitacoder2004@gmail.com>
2020-11-29 15:20:43 +0530

    fake images added but not visible

--- ankita/listing/views.py
+++ ankita/listing/views.py
@@ -7,17 +7,17 @@ def index(request):
 
     dest1 = Destination()
     dest1.name = 'Mumbai'
-    dest1.img = 'img-1.jpg'
+    dest1.img = 'mumbai.jpg'
     dest1.price = 700
 
     dest2 = Destination()
     dest2.name = 'Hyderabad'
-    dest2.img = 'img-2.jpg'
+    dest2.img = 'Hyderabad.jpg'
     dest2.price = 650
 
     dest3 = Destination()
     dest3.name = 'Bengaluru'
-    dest3.img = 'img-3.jpg'
+    dest3.img = 'bengaluru.jpg'
     dest3.price = 750
 
     dests = [dest1, dest2, dest3]
