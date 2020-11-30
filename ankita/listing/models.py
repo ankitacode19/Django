72cd8955d1f7a05c850c6357083a2044c796aa1f
ankitacode19 <ankitacoder2004@gmail.com>
2020-11-30 14:55:39 +0530

    0002 created

--- ankita/listing/models.py
+++ ankita/listing/models.py
@@ -6,5 +6,5 @@ class Destination(models.Model):
 
     name = models.CharField(max_length = 100)
     img = models.ImageField(upload_to = 'images')
-    price = models.IntegerField
+    price = models.IntegerField()
     offer = models.BooleanField(default = False)
